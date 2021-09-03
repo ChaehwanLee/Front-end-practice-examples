16c9c77bbaa619c27789f502e573c1235e56a6c7
ChaehwanLee <oasisinyourmind@gmail.com>
2021-09-04 03:47:01 +0900

    Javascript

---
+++ Javascript/main.js
@@ -0,0 +1,72 @@
+// comic 박스를 생성한 상태
+// 변수선언
+// var comic;
+
+// comic 박스 안에 원피스 데이터를 할당한 상태
+// 변수 초기화
+// comic = "원피스";
+
+// console.log(comic);
+
+//var apple = "사과";
+
+/*
+var a;
+var b;
+var c;
+*/
+
+// 변수 동시 선언
+// var a, b, c;
+
+// 변수 동시 초기화
+/*
+var d = 10;
+var e = 20;
+var f = 30;
+*/
+
+//var d = 10, e = 20, f = 30;
+
+
+// 모든 변수는 선언과 동시에 undefined 자동으로 할당된다.
+// var computer;
+// console.log(computer);
+
+/*
+var house;
+house = "집";
+console.log(house);
+
+console.log(house);
+house = "아파트";
+*/
+
+/*
+var house;
+console.log(house);
+
+// undefined -> 집
+house = "집"
+console.log(house);
+*/
+
+
+// 캐멀 케이스
+// var mathScoreStudent = 10;
+
+// 스네이크 케이스
+// var math_score_student = 10;
+
+// 주의점 4가지
+// var math-score-student = 100; // 케밥케이스 사용 불가 error
+
+// var _math;
+// var $math;
+
+// var apple10;
+// var 10apple; 숫자가 먼저 나와서는 안된다
+
+//var w; 의미 불분명한 변수 기피, 가능하면 자세히 적어주는 것이 좋다
+
+// 데이터 타입
